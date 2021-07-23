eebac534187e2699e4e8f0326f34dffabd63ee75
Imatrei <87873892+Imatrei@users.noreply.github.com>
2021-07-23 12:13:57 -0700

    Web Scraping Test

---
+++ caption.py
@@ -0,0 +1,8 @@
+import os
+import json
+
+url = os.getenv('QUERY_STRING')
+url = json.loads(url)
+url = "https://" + url + "&fmt=json3"
+
+print(url)
---
+++ test.py
@@ -0,0 +1,30 @@
+import os
+import urllib.parse
+import webbrowser
+
+code = """
+var regexp = new RegExp(/playerCaptionsTracklistRenderer.*?(youtube.com\/api\/timedtext.*?)"/);
+var match = regexp.exec(document.body.innerHTML);
+if (!match) {
+  alert ("No captions found");
+  return;
+}
+var url = regexp.exec(document.body.innerHTML)[1];
+var new_window = open("http://Imatrei.github.io/ko-en-card-generator/caption.py?url=" + encodeURIComponent(url));
+"""
+
+code = code.replace("$url", encodedURL)
+code = urllib.parse.quote(code)
+
+html = """
+<a href="javascript:(function(){ $code })()">YouTube Transcription</a>
+"""
+
+html = html.replace("$code", code)
+
+path = os.path.abspath('test.html')
+url = 'file://' + path
+
+with open(path, 'w') as f:
+    f.write(html)
+webbrowser.open(url)
